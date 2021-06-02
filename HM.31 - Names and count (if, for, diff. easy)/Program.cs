d5d3f07f5687e74686c66ed4e52069f6e592b3d8
valerijamurzina <valerijamurzina@gmail.com>
2021-06-02 17:34:54 +0100

    Initial project files

---
+++ HM.31 - Names and count (if, for, diff. easy)/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace HM._31___Names_and_count__if__for__diff._easy_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Create console application, where string array is declared with (n) names.
+            //The program should calculate, how many names start with particular letter. 
+            //Example, we have names Jānis, Juris, Gatis, Gundars, Andris -
+            //as a result we should get info J=2, G=2, A=1
+
+            Console.WriteLine("Hello World!");
+
+        }
+    }
+}
